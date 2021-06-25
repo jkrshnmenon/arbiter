ae1a6228c6cd87b2c1e78b36527d6a300e8cb225
Jay <jkrshnmenon@gmail.com>
2021-06-25 15:52:04 +0530

    shorten class names

--- arbiter/master_chief/__init__.py
+++ arbiter/master_chief/__init__.py
@@ -1,3 +1,3 @@
-from .sa_recon import StaticAnalysisRecon
-from .sa_advanced import StaticAnalysisAdvanced
-from .symbolic_execution import SymbolicExecution
+from .sa_recon import SA_Recon
+from .sa_advanced import SA_Adv
+from .symbolic_execution import SymExec
