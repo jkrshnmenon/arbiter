f11a2292bbcad8927030e068c3bf36041b9331d1
Jayakrishna Menon <jkrshnmenon@gmail.com>
2022-08-29 16:13:56 -0700

    updating test case to use correct binaries

--- test_files/test_arbiter.py
+++ test_files/test_arbiter.py
@@ -8,13 +8,23 @@ from arbiter import VDGraph
 from arbiter import FirstArg, RetNode
 
 class ArbiterTest(unittest.TestCase):
-    def test_simple_example(self):
+    def test_single_func_data_flow(self):
         vd = VDGraph()
         src = RetNode('strlen')
         dst = FirstArg('malloc')
         vd.add_edge(src, dst)
 
-        p = Arbiter(filename=Path(__file__ ).parent / 'simple_example.elf', vd=vd)
+        p = Arbiter(filename=Path(__file__ ).parent / 'build/single_func_data_flow.elf', vd=vd)
+        recon = Recon(p.storage)
+        recon.analyze_all()
+    
+    def test_multi_func_data_flow(self):
+        vd = VDGraph()
+        src = RetNode('strlen')
+        dst = FirstArg('malloc')
+        vd.add_edge(src, dst)
+
+        p = Arbiter(filename=Path(__file__ ).parent / 'build/multi_func_data_flow.elf', vd=vd)
         recon = Recon(p.storage)
         recon.analyze_all()
 
