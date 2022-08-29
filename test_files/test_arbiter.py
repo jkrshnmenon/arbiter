126d3ec7d839d17e97b0a4951b8558af170a6cfc
Jayakrishna <jkrshnmenon@gmail.com>
2022-08-29 11:07:53 -0700

    test cases

---
+++ test_files/test_arbiter.py
@@ -0,0 +1,22 @@
+import unittest
+
+from pathlib import Path
+
+from arbiter import Recon
+from arbiter import Arbiter
+from arbiter import VDGraph
+from arbiter import FirstArg, RetNode
+
+class ArbiterTest(unittest.TestCase):
+    def test_simple_example(self):
+        vd = VDGraph()
+        src = RetNode('strlen')
+        dst = FirstArg('malloc')
+        vd.add_edge(src, dst)
+
+        p = Arbiter(filename=Path(__file__ ).parent / 'simple_example', vd=vd)
+        recon = Recon(p.storage)
+        sources, sinks = recon.analyze_all()
+        print(sources)
+        print(sinks)
+
