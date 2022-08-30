fe46d2a5002b4091d56969a9c090288e3c8d104c
Jayakrishna <jkrshnmenon@gmail.com>
2022-08-30 10:31:22 -0700

    new testcase file

---
+++ test_files/multi_data_flow.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char *multi_data_flow(char *input) {
+	int x = strlen(input);
+	char *p = (char *)malloc(x);
+    memcpy(p, input, x);
+	return p;
+}
+
+
+int main() {
+	char *ptr = multi_data_flow("AAAAAAA");
+	return 0;
+}
+
