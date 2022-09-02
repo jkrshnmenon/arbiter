6851f779ee59b4a389e9652ef0ccca08bcd4fa1e
Jayakrishna <jkrshnmenon@gmail.com>
2022-09-02 11:40:37 -0700

    datamarker and dataresolution classes

--- arbiter/storage/__init__.py
+++ arbiter/storage/__init__.py
@@ -1,2 +1,3 @@
 from .storage import Storage
 from .control_flow_detection import SinkMarker, SinkFlow, M, S
+from .data_flow_detection import DataResolution, DataMarker, DR
--- arbiter/storage/data_flow_detection.py
+++ arbiter/storage/data_flow_detection.py
@@ -6,22 +6,47 @@ from ..spec import VDNode
 
 from .control_flow_detection import M, SinkMarker
 
+B = Type[angr.block.Block]
 
-class Marker(object):
-    def __init__(self, marker: M):
-        """Base class for storing data flow results
+
+
+class DataResolution(object):
+    def __init__(self, block: B, node: N) -> None:
+        """Class to store the details of a data-flow for a vd node
 
         Args:
-            marker (M): The SinkMarker object
-
-        Raises:
-            NotImplementedError: This class should not be used directly
+            block (B): The angr basic block object
+            node (N): The VD node
         """
-        self._marker = marker
-        raise NotImplementedError
+        self._block = block
+        assert isinstance(node, VDNode)
+        assert not isinstance(node, MetaNode), "MetaNode not supported in DataResolution class"
+        self._node = node
+
+        # self._resolution = {'insn_addr': None, 'insn': None, 'vex_idx': None, 'vex_stmt': None}
+        self._resolution = resolve_data_marker(block=self._block, vd_node=node)
+    
+    @property
+    def insn(self):
+        return self._resolution['insn']
+    
+    @property
+    def insn_idx(self):
+        return self._resolution['insn_idx']
+    
+    @property
+    def vex_stmt(self):
+        return self._resolution['vex_stmt']
+    
+    @property
+    def vex_idx(self):
+        return self._resolution['vex_idx']
 
 
-class DataMarker(Marker):
+DR = Type[DataResolution]
+
+
+class DataMarker():
     def __init__(self, marker: M) -> None:
         """
         Args:
@@ -29,10 +54,32 @@ class DataMarker(Marker):
         """
         self._marker = marker
         assert isinstance(self._marker, SinkMarker)
-        self._vd_node = marker.marker
-        assert isinstance(self._vd_node, VDNode)
-        self._block = marker.block
+        self._node = marker.marker
+        assert isinstance(self._node, VDNode)
+        self._block = self._marker.block
 
-        self.vex_stmt_idx = resolve_data_marker(block=self._block, vd_node=self._vd_node)
+        if isinstance(self._node, MetaNode):
+            self._data = {x: None for x in self._node.nodes}
+        else:
+            self._data = {self._node: None}
+        
+        for x in self._data:
+            self._data[x] = DataResolution(block=self.block, node=x)
+    
+    @property
+    def vd_nodes(self) -> list[N]:
+        return list(self._data.keys())
+    
+    @property
+    def block(self) -> B:
+        return self._block
+    
+    @property
+    def marker(self) -> M:
+        return self._marker
+    
+    def resolved_marker(self, vd_node: N) -> DR:
+        assert vd_node in self._data
+        return self._data[vd_node]
     
     
