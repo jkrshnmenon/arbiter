bdf643c6390c94513a8fc3e0dfda0d2ac4133dd6
Jayakrishna Menon <jkrshnmenon@gmail.com>
2022-09-02 16:25:22 -0700

    initial draft for data flow

--- arbiter/analysis/data_flow.py
+++ arbiter/analysis/data_flow.py
@@ -5,3 +5,38 @@ from typing import Type, List, Dict
 
 from ..storage import *
 from ..spec import *
+
+
+
+class DataFlow(object):
+    """A class to perform the DataFlow analysis across SinkFlows
+    """
+    def __init__(self, storage: S) -> None:
+        """
+
+        Args:
+            storage (S): The Arbiter Storage object
+        """
+        self.storage = storage
+    
+    @property
+    def cfg(self):
+        return self.storage.cfg
+    
+    @property
+    def vd(self):
+        return self.storage.vd
+    
+    def resolve_data_flow(self, flow: S):
+        """Resolve a data flow backwards from sink to source
+
+        Args:
+            flow (S): A SinkFlow object
+        """
+        sources, sinks = flow.sources, flow.sinks
+    
+    def analyze_all(self):
+        """Analyze all of the SinkFlows
+        """
+        for flow in self.storage.iter_sinks():
+            self.resolve_data_flow(flow)
