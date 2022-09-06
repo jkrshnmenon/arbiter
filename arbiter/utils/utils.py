be895e0efc3750db14dd8a3213eb23b82a78dc79
Jayakrishna <jkrshnmenon@gmail.com>
2022-09-05 17:20:39 -0700

    implementing data resolution for RetNode

--- arbiter/utils/utils.py
+++ arbiter/utils/utils.py
@@ -10,6 +10,9 @@ def prGreen(skk):
 def is_reg_write(stmt):
     return stmt.tag == 'Ist_Put'
 
+def is_reg_read(stmt):
+    return stmt.tag == 'Iex_Get'
+
 def is_imark(stmt):
     return stmt.tag == 'Ist_IMark'
 
@@ -22,7 +25,10 @@ def target_reg_offset(stmt):
 def vex_is_reg_write(stmt, reg_vex_offset: int):
     return is_reg_write(stmt) and target_reg_offset(stmt) == reg_vex_offset
 
-def resolve_data_marker(block, vd_node) -> dict:
+def vex_is_reg_read(stmt, reg_vex_offset: int):
+    return is_reg_read(stmt) and target_reg_offset(stmt) == reg_vex_offset
+
+def resolve_data_marker(block, vd_node, function) -> dict:
     assert isinstance(block, angr.block.Block)
     assert isinstance(vd_node, VDNode)
 
@@ -32,8 +38,39 @@ def resolve_data_marker(block, vd_node) -> dict:
     vex_stmt = None
 
     if isinstance(vd_node, RetNode):
-        # Get the vex id of instruction from next block
-        pass
+        # Convert block to function blocknode
+        blocknode = None
+        for tmp in function.graph.nodes:
+            if tmp.addr == block.addr:
+                blocknode = tmp
+                break
+        assert blocknode is not None
+
+        # Get the successor block
+        out_edges = function.graph.out_edges(blocknode)
+        assert len(out_edges) == 1
+
+        _, next_blocknode = list(out_edges)[0]
+
+        # Convert blocknode back to block
+        for tmp in function.blocks:
+            if tmp.addr == next_blocknode.addr:
+                block = tmp
+                break
+        
+        # Find the instruction that reads from the return register
+        register_vex_offset: int = vd_node.register_offset
+        for idx, stmt in enumerate(block.vex.statements):
+            if is_imark(stmt):
+                insn_addr = stmt_addr(stmt)
+            if not hasattr(stmt, "data"):
+                continue
+            if vex_is_reg_read(stmt.data, register_vex_offset):
+                vex_id = idx
+                vex_stmt = stmt
+                # Break because we want the first instance here
+                break
+
     elif isinstance(vd_node, EOFNode):
         # Get the vex id of instruction that sets return_register
         pass
@@ -45,6 +82,7 @@ def resolve_data_marker(block, vd_node) -> dict:
             if vex_is_reg_write(stmt, register_vex_offset):
                 vex_id = idx
                 vex_stmt = stmt
+                # No break here because we want the last
         
     for ins in block.capstone.insns:
         if ins.address == insn_addr:
